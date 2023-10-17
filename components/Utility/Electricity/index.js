1634e4ea3790885c1e0a075580f2fed5873759b9
haybuka <paschal.nduleo@gmail.com>
2023-10-17 16:09:36 +0100

    svg cleanup

--- components/Utility/Electricity/index.js
+++ components/Utility/Electricity/index.js
@@ -1,34 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FlatList, Pressable, Text, View } from 'react-native'
 import ArrowRight from '../../../assets/icon/ArrowRight'
 import { useNavigation } from '@react-navigation/native'
 
-const Electricity = ({ handleTitle }) => {
+const data = [
+  {
+    label: "ikeja electric",
+    name: "ikeja",
+    image: "",
+    id: 1
+  },
+  {
+    label: "ibadan electric",
+    name: "ibadan",
+    image: "",
+    id: 2
+  },
+  {
+    label: "eko electric",
+    name: "eko",
+    image: "",
+    id: 3
+  }
+]
+
+const Electricity = () => {
   const navigation = useNavigation()
-  const data = [
-    {
-      label: "ikeja electric",
-      name: "ikeja",
-      image: "",
-      id: 1
-    },
-    {
-      label: "ibadan electric",
-      name: "ibadan",
-      image: "",
-      id: 2
-    },
-    {
-      label: "eko electric",
-      name: "eko",
-      image: "",
-      id: 3
-    }
-  ]
+  //  const [pageData,setPageData] = useState()
 
   const handleTitleChange = (item) => {
     // navigation.push('Utility')
-    navigation.navigate("Payment")
+    navigation.navigate("Payment", {
+      item
+    })
   }
 
   return (
@@ -38,7 +42,7 @@ const Electricity = ({ handleTitle }) => {
         keyExtractor={(data) => data.id.toString()}
         ItemSeparatorComponent={<View className="my-3"></View>}
         renderItem={({ item }) => (
-          <Pressable onPress={handleTitleChange} className="border border-gray-300 flex-row items-center rounded-md p-3 py-4">
+          <Pressable onPress={() => handleTitleChange(item)} className="border border-gray-300 flex-row items-center rounded-md p-3 py-4">
             <View className="flex-1 ">
               <Text className="capitalize font-semibold text-[16px]">{item.label}</Text>
             </View>
